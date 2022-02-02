6b2d403ec92ab3efcf198a2f5c57e4dbd52d5f41
itrytocodethings <imwaynebailey@gmail.com>
2022-02-02 18:18:03 -0500

    completed traffic light

--- src/js/component/app.js
+++ src/js/component/app.js
@@ -1,6 +1,31 @@
-import React from "react";
-import { Trafficlight } from "./traffic_light";
+import React, { useState } from "react";
 
 export const App = () => {
-	return <Trafficlight />;
+	const [color, setColor] = useState("");
+	if (color) {
+		document.querySelector(`#${color}`).classList.add(`selected-${color}`);
+	}
+
+	return (
+		<div className="container">
+			<div className="traffic-light mt-3 d-flex justify-content-center flex-column align-items-center">
+				<div className="">{color}</div>
+				<div className="traffic-light-top"></div>
+				<div
+					className="traffic-light-bottom p-2"
+					onClick={(e) => {
+						for (let item of e.target.parentNode.children) {
+							item.classList.contains(`selected-${color}`)
+								? item.classList.remove(`selected-${color}`)
+								: null;
+						}
+						setColor(e.target.id);
+					}}>
+					<div id="red" className="light red"></div>
+					<div id="yellow" className="light yellow"></div>
+					<div id="green" className="light green"></div>
+				</div>
+			</div>
+		</div>
+	);
 };
