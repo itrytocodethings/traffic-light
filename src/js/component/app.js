6ba3b22486c4c432639fc1dd19d9218a37aeb10a
itrytocodethings <imwaynebailey@gmail.com>
2022-02-04 17:57:17 -0500

    updated logic for light selected

--- src/js/component/app.js
+++ src/js/component/app.js
@@ -2,28 +2,40 @@ import React, { useState } from "react";
 
 export const App = () => {
 	const [color, setColor] = useState("");
-	if (color) {
-		document.querySelector(`#${color}`).classList.add(`selected-${color}`);
-	}
-
 	return (
 		<div className="container">
 			<div className="traffic-light mt-3 d-flex justify-content-center flex-column align-items-center">
-				<div className="">{color}</div>
+				<div className="mb-3" style={{ color: `${color}` }}>
+					<h1>{color}</h1>
+				</div>
 				<div className="traffic-light-top"></div>
 				<div
-					className="traffic-light-bottom p-2"
+					className="traffic-light-bottom p-3 rounded"
 					onClick={(e) => {
-						for (let item of e.target.parentNode.children) {
-							item.classList.contains(`selected-${color}`)
-								? item.classList.remove(`selected-${color}`)
-								: null;
+						if (e.target.id && e.target.id != color) {
+							for (let item of e.target.parentNode.children) {
+								item.classList.contains(`selected-${color}`)
+									? item.classList.remove(`selected-${color}`)
+									: null;
+							}
+							setColor(e.target.id);
 						}
-						setColor(e.target.id);
 					}}>
-					<div id="red" className="light red"></div>
-					<div id="yellow" className="light yellow"></div>
-					<div id="green" className="light green"></div>
+					<div
+						id="red"
+						className={`light red ${
+							color == "red" ? `selected-red` : null
+						}`}></div>
+					<div
+						id="yellow"
+						className={`light yellow ${
+							color == "yellow" ? `selected-yellow` : null
+						}`}></div>
+					<div
+						id="green"
+						className={`light green ${
+							color == "green" ? `selected-green` : null
+						}`}></div>
 				</div>
 			</div>
 		</div>
